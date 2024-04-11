f3a73768c21f212b4c42a0a3b60ad6923197bcc1
Alexander Petrovski <apfb11@gmail.com>
2024-04-11 17:05:37 -0400

    new resume

--- scripts/cvbutton.js
+++ scripts/cvbutton.js
@@ -8,7 +8,7 @@ export default class ButtonCreator {
     a.target = "_blank";
     a.rel = "noopener";
     a.href =
-      "https://drive.google.com/file/d/1cp6rbbBG9ejFHt5c9IErs_nJuy_vhYDx/view?usp=drive_link";
+      "https://drive.google.com/file/d/1Sj-SdH2F1n9l76-L6hb0GD8vDW0Sf8OJ/view?usp=drive_link";
 
     const spanText = document.createElement("span");
     spanText.classList.add("text-btn");
